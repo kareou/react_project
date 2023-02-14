ef35ff02827e7ae4cbe00a288758a28a839ff4d5
mohamed khairoun <102796816+kareou@users.noreply.github.com>
2023-02-14 12:11:06 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+//import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import About from './component/about';
-import Home from './component/home';
-import Profile from './component/profile';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './component/navbar';
-// import Nopage from './component/nopage';
+import About from './component/about';
+import Login from './component/login';
+import Profile from './component/profile';
+import Service from './component/service';
+import Home from './component/home';
 
 function App() {
   return (
@@ -12,12 +14,12 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/about' element={<About />} />
-          {/* <Route path='*' element={<Nopage />} /> */}
+          <Route path='/' element ={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/service' element={<Service/>}/>
+          <Route path='about' element={<About/>}/>
+          <Route path='element' element={<Login/>}/>
         </Routes>
-        <h1>this is a footer</h1>
       </Router>
     </div>
   );
